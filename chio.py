5f74cd3773568743428dcef44f62e633574ace82
Stefan Kuntz <stefan.github@gmail.com>
2020-10-11 19:34:22 +0200

    logging

--- chio.py
+++ chio.py
@@ -1,10 +1,14 @@
-import subprocess
 import re
+import subprocess
+import logging
 
 CHIO_CMD = 'chio'
 name_pattern = r'(?P<name>[a-zA-Z]+\s[0-9]+):(\s+<(?P<status>\S+)>)?'
 voltag_pattern = r'.*?(\svoltag:\s<(?P<voltag>\S+):\S*?>)'  # explicit \s required to not match avoltag
 source_pattern = r'(source:\s<(?P<source>(?!>).+?)>)'
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 
 def parse_chio_status_line(line):
@@ -38,20 +42,26 @@ def chio_options(device=None):
 
 
 def status(device=None):
+    logger.debug(f'Getting autochanger status from device {device}')
     status_options = ['-a']
     result = subprocess.run(chio_options(device) + ['status'] + status_options, capture_output=True)
+    logger.debug(f'Autochanger status exited with code {result.returncode}: {result.stderr}')
     result.check_returncode()
     return parse_chio_status(result.stdout.decode('utf-8').split('\n'))
 
 
 def load(volume, device=None, drive_index=0):
+    logger.debug(f'Loading volume {volume} into drive {drive_index} on device {device}')
     result = subprocess.run(chio_options(device) + ['move', 'voltag', volume, 'drive', str(drive_index)],
                             capture_output=True)
+    logger.debug(f'Load volume command exited with code {result.returncode}: {result.stderr}')
     result.check_returncode()
 
 
 def unload(device=None, drive_index=0):
+    logger.debug(f'Unloading volume in drive {drive_index} on device {device}')
     result = subprocess.run(chio_options(device) + ['return', 'drive', str(drive_index)], capture_output=True)
+    logger.debug(f'Unload volume command exited with code {result.returncode}: {result.stderr}')
     result.check_returncode()
 
 
--- db.py
+++ db.py
@@ -1,18 +1,23 @@
 import sqlite3
 stat_fields = ['mode', 'dev', 'nlink', 'uid', 'gid', 'size', 'atime', 'mtime', 'ctime']
 
+import logging
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
 def create_tables(cursor):
     stat = [f"`st_{field}` INTEGER" for field in stat_fields]
     delim = ',\n'
     sql = f"""
     CREATE TABLE IF NOT EXISTS `files` (
         `id` INTEGER,
-        `filepath`	TEXT NOT NULL,
+        `path`	TEXT NOT NULL,
         `st_ino`	INTEGER NOT NULL,
         `sha1`	BLOB,
+        `is_dir` INTEGER,
         {delim.join(stat)},
         PRIMARY KEY(`id`),
-        UNIQUE(`filepath`, `st_ino`, `sha1`)
+        UNIQUE(`path`, `st_ino`, `sha1`)
     );
     CREATE INDEX IF NOT EXISTS file_index ON files (sha1, filepath, st_ino);
     CREATE TABLE IF NOT EXISTS `volumes` (
@@ -25,14 +30,17 @@ def create_tables(cursor):
         PRIMARY KEY(`voltag`)
         );
     """
+    logger.debug(f'Creating DB tables if they do not already exist. Using stat fields: {", ".join(stat_fields)}.')
     cursor.executescript(sql)
 
 
 def init_db(db_file):
+    logger.info(f'Initializing sqlite database {db_file}.')
     conn = sqlite3.connect(db_file)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
     create_tables(c)
+    logger.debug('Done initializing sqlite database.')
     return conn
 
 
